c43f51a657437dadb5e6797b407138578d79e510
N0wah <noah68mollet@gmail.com>
2023-09-22 15:39:27 +0200

    pendu

--- pendu.go
+++ pendu.go
@@ -12,19 +12,96 @@ func main() {
 	runeAffichage := []rune(affichage)
 	trouve := 0
 	coups := 0
+	erreur := 0
 	for trouve != len(reponse) {
 		var rep string
 		fmt.Println(string(runeAffichage))
 		fmt.Scanln(&rep)
+		trouveReg := trouve
 		for i := range runeReponse {
 			if rep == string(runeReponse[i]) {
 				runeAffichage[i] = runeReponse[i]
 				trouve++
 			}
 		}
+		if trouveReg == trouve {
+			erreur++
+		}
 		coups++
+		if erreur == 1 {
+			Dessin1()
+		} else if erreur == 2 {
+			Dessin2()
+		} else if erreur == 3 {
+			Dessin3()
+		} else if erreur == 4 {
+			Dessin4()
+		} else if erreur == 5 {
+			Dessin5()
+			break
+		}
 	}
 	fmt.Println(string(runeAffichage))
-	fmt.Printf("Tu as trouvé la réponse en ")
-	fmt.Println(coups)
+	if string(runeAffichage) == string(runeReponse) {
+		fmt.Printf("Tu as trouvé la réponse en ")
+		fmt.Println(coups)
+	} else {
+		fmt.Println("Tu n'as pas trouvé, dommage !")
+	}
+}
+
+func Dessin1() {
+	fmt.Println("        ")
+	fmt.Println("        ")
+	fmt.Println("        ")
+	fmt.Println("        ")
+	fmt.Println("        ")
+	fmt.Println("        ")
+	fmt.Println("        ")
+	fmt.Println("________")
+}
+
+func Dessin2() {
+
+	fmt.Println("       |")
+	fmt.Println("       |")
+	fmt.Println("       |")
+	fmt.Println("       |")
+	fmt.Println("       |")
+	fmt.Println("       |")
+	fmt.Println("       |")
+	fmt.Println("       |")
+	fmt.Println("________")
+}
+func Dessin3() {
+	fmt.Println(" _______|")
+	fmt.Println("        |")
+	fmt.Println("        |")
+	fmt.Println("        |")
+	fmt.Println("        |")
+	fmt.Println("        |")
+	fmt.Println("        |")
+	fmt.Println("_________")
+}
+
+func Dessin4() {
+	fmt.Println("_______|")
+	fmt.Println("   |   |")
+	fmt.Println("   0   |")
+	fmt.Println("       |")
+	fmt.Println("       |")
+	fmt.Println("       |")
+	fmt.Println("       |")
+	fmt.Println("________")
+}
+
+func Dessin5() {
+	fmt.Println(" _______|")
+	fmt.Println("    |   |")
+	fmt.Println("    0   |")
+	fmt.Println("   \\|/  |")
+	fmt.Println("    |   |")
+	fmt.Println("   / \\  |")
+	fmt.Println("        |")
+	fmt.Println("_________")
 }
